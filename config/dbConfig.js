73df95f52d69bb5a3f93648b4fc098f382c2cbd2
joe342wise <jnyarko342@gmail.com>
2024-06-29 15:27:20 +0000

    chore: Add MySQL database configuration file

---
+++ config/dbConfig.js
@@ -0,0 +1,15 @@
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '123456789',
+    database: 'blogPlatform'
+});
+
+connection.connect((err) => {
+    if(err) throw err;
+    console.log('Connected to the database')
+});
+
+module.exports = connection
